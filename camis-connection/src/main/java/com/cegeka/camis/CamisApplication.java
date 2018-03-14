71524aba2339075c94d1b814c49240741c91e645
seppeg <gielenseppe@gmail.com>
2018-03-14 13:56:27 +0100

    seppeg: Simple context loading

---
+++ camis-connection/src/main/java/com/cegeka/camis/CamisApplication.java
@@ -0,0 +1,13 @@
+package com.cegeka.camis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamisApplication.class, args);
+    }
+
+}
---
+++ camis-connection/src/test/java/com/cegeka/camis/ContextLoadTest.java
@@ -0,0 +1,12 @@
+package com.cegeka.camis;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ContextLoadTest {
+    @Test
+    void checkIfContextCouldBeLoaded() {
+    }
+}
