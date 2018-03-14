1f5d8a8425e1f97f252b32fa76aeca2e04ef929a
seppeg <gielenseppe@gmail.com>
2018-03-14 14:08:35 +0100

    seppeg: upgrading gradle, jdk problem

--- camis-connection/src/test/java/com/cegeka/camis/ContextLoadTest.java
+++ camis-connection/src/test/java/com/cegeka/camis/ContextLoadTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-public class ContextLoadTest {
+class ContextLoadTest {
     @Test
     void checkIfContextCouldBeLoaded() {
     }
