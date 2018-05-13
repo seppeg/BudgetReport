35ab0ba13f6124f59667e5ea5c1d8d83ecadd8bb
seppeg <gielenseppeg@gmail.com>
2018-05-13 13:57:27 +0200

    seppeg: added employee and frontend

---
+++ employee/src/test/java/com/cegeka/employee/ContextLoadTest.java
@@ -0,0 +1,12 @@
+package com.cegeka.employee;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class ContextLoadTest {
+    @Test
+    void checkIfContextCouldBeLoaded() {
+    }
+}
