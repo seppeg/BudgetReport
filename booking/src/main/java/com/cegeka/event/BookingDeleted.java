a1eed58442aaa6545705452b04fe8bd32c6f250a
seppeg <seppe.gielen@cegeka.com>
2018-05-18 09:10:11 +0200

    seppeg: added BookingDeleted event

---
+++ booking/src/main/java/com/cegeka/event/BookingDeleted.java
@@ -0,0 +1,18 @@
+package com.cegeka.event;
+
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Value
+public class BookingDeleted {
+
+    private final UUID id;
+    private String workorder;
+    private LocalDate date;
+    private String description;
+    private double hours;
+    private String employee;
+
+}
---
+++ booking/src/main/java/com/cegeka/event/BookingDeletedBuilder.java
@@ -0,0 +1,54 @@
+package com.cegeka.event;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public final class BookingDeletedBuilder {
+    private UUID id;
+    private String workorder;
+    private LocalDate date;
+    private String description;
+    private double hours;
+    private String employee;
+
+    private BookingDeletedBuilder() {
+    }
+
+    public static BookingDeletedBuilder deletedBooking() {
+        return new BookingDeletedBuilder();
+    }
+
+    public BookingDeletedBuilder id(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public BookingDeletedBuilder workorder(String workorder) {
+        this.workorder = workorder;
+        return this;
+    }
+
+    public BookingDeletedBuilder date(LocalDate date) {
+        this.date = date;
+        return this;
+    }
+
+    public BookingDeletedBuilder description(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public BookingDeletedBuilder hours(double hours) {
+        this.hours = hours;
+        return this;
+    }
+
+    public BookingDeletedBuilder employee(String employee) {
+        this.employee = employee;
+        return this;
+    }
+
+    public BookingDeleted build() {
+        return new BookingDeleted(id, workorder, date, description, hours, employee);
+    }
+}
